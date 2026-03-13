2b1d324965aa7fde2b5e7acf9b459649b07bbf70
Nicolas Gigot <nicolas.gigot@protonmail.com>
2026-03-13 14:14:29 +0100

    config: Dotenv setup

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,7 +1,9 @@
 import express from 'express';
+import config from 'dotenv/config';
+
 
 const app = express();
 
 app.get('/', (req, res) => res.send('hello'));
 
-app.listen(80, () => console.log('serveur à l\'écoute'));
+app.listen(process.env.PORT, () => console.log('serveur à l\'écoute'));
