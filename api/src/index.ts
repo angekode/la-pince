0f95c231e727ffe03193a7094fb4df9e9e54ffbf
Nicolas Gigot <nicolas.gigot@protonmail.com>
2026-03-13 15:21:55 +0100

    config: prisma setup

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,9 +1,13 @@
 import express from 'express';
 import config from 'dotenv/config';
+import { prisma } from './db/prisma-client.ts';
 
 
+// Création de l'application express
 const app = express();
-
 app.get('/', (req, res) => res.send('hello'));
-
 app.listen(process.env.PORT, () => console.log('serveur à l\'écoute'));
+
+// Test prisma
+await prisma.$connect();
+console.log("DB: connected");
