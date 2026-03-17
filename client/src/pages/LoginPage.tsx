eb9ef3b35cae5f2ae7109502d20e4b13832c87ca
M.M <medwelcome093@gmail.com>
2026-03-17 13:24:06 +0100

    General-Form-Error

---
+++ client/src/pages/LoginPage.tsx
@@ -0,0 +1,103 @@
+import { useState } from 'react';
+
+function LoginPage() {
+
+    const [pseudo, setPseudo] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const [error, setError] = useState(false);
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+        console.log(pseudo, email, password);
+
+        if (!pseudo || !email || !password){
+        setError(true)
+        return
+        }
+
+        setError(false)
+        
+    }
+
+  return (
+    <div className="auth-page">
+      <header className="auth-header">
+        <div className="auth-logo">LOGO</div>
+      </header>
+
+      <main className="auth-main">
+        <section className="auth-card">
+          <div className="auth-tabs">
+            <button className="auth-tab active" type="button">
+              Connexion
+            </button>
+            <button className="auth-tab" type="button">
+              Inscription
+            </button>
+          </div>
+
+
+          <form className="auth-form" onSubmit={handleSubmit}>
+            <div className="form-group">
+                <label htmlFor="pseudo">Pseudonyme</label>
+                <input 
+                    id="pseudo"
+                    type="text"
+                    placeholder="Oclock2026"
+                    value={pseudo}
+                    onChange={(event) => setPseudo(event.target.value) } 
+                />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="email">Adresse mail</label>
+              <input 
+                id="email"
+                type="email" 
+                placeholder="oclock@mail.com"
+                value={email}
+                onChange={(event) => setEmail(event.target.value)}
+                 />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="password">Mot de passe</label>
+              <input
+                id="password" 
+                type="password" 
+                placeholder="**********"
+                value={password}
+                onChange={(event) => setPassword(event.target.value)}
+                 />
+            </div>
+
+            <button className="primary-button" type="submit">
+              Valider
+            </button>
+
+            {error && <p className="error-message">Tous les champs sont obligatoires</p>}
+          </form>
+
+        </section>
+
+        <section className="primary-button">
+            
+            <button className="acceuil-button" onClick="window.location.href='https://example.com';">Acceuil</button>
+        
+        </section>
+        
+
+      </main>
+
+      <footer className="auth-footer">
+        <a href="/">RGPD</a>
+        <a href="/">CGU</a>
+        <a href="/">Mentions légales</a>
+      </footer>
+    </div>
+  )
+}
+
+export default LoginPage
