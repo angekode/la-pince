66d704e39a0cd593eaa3b8db3142a5e15901a06a
M.M <medwelcome093@gmail.com>
2026-03-25 20:53:14 +0100

    Home-Section

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,25 +1,56 @@
 import HeaderHome from "../components/Header-home";
+import HomeSlide from "../components/HomeSlide";
+import "../styles/home.css"
+import { useNavigate } from "react-router-dom";
 
 function Home() {
+  const navigate = useNavigate();
   return (
     <div>
+
+      {/* Header de la page Home */}
       <HeaderHome />
 
       <main className="home-main">
+
+        {/* Section présentation des slides */}
         <section id="gestion-budget" className="home-section">
-          <h2>Gestion de budget</h2>
-          <p>Contenu de la section gestion de budget.</p>
+          <HomeSlide />
         </section>
 
-        <section id="visualisation" className="home-section">
-          <h2>Visualisation</h2>
-          <p>Contenu de la section visualisation.</p>
-        </section>
-
+        {/* Section Cybersecurité */}
         <section id="securite" className="home-section">
-          <h2>Sécurité</h2>
-          <p>Contenu de la section sécurité.</p>
+          <div className="security-section">
+    
+            <h2>Sécurité Avancé</h2>
+
+            <div className="security-points">
+              <p>🔒 Vos données sont chiffrées et protégées.</p>
+              <p>🛡️ Connexion sécurisée avec authentification.</p>
+              <p>📊 Respect total de votre vie privée.</p>
+            </div>
+
+            <div className="security-image">
+              <img src="/DesktopShield.png" alt="sécurité" />
+            </div>
+
+          </div>
         </section>
+
+        {/* Section proposition d'inscription */}
+        <section id="inscription" className="home-section">
+          <div className="signup-section">
+
+            <h2>Alors convaincu ?</h2>
+
+            <p>Inscrivez-vous gratuitement en quelques secondes et commencez à mieux gérer votre budget dès aujourd’hui.</p>
+
+            <button className="primary-button signup-button" onClick={() => navigate("/register")}>
+              Créer un compte
+            </button>
+          </div>
+        </section>
+
       </main>
     </div>
   );
